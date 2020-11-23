9efcaab75dfd6f69c85a6e09ae706695c12d7d7d
Felipe Castillo López <fesecalo@gmail.com>
2020-11-23 05:46:56 -0300

    home y stage1

---
+++ assets/js/javascript.js
@@ -0,0 +1,17 @@
+function initParpadear(stage)
+{
+  window.setInterval (BlinkIt, 200);
+  var color = "#980ab0";
+  
+  function BlinkIt ()
+  {
+    var blink = document.getElementById (stage);
+    color = (color == "#ffffff")? "#980ab0" : "#ffffff";
+    blink.style.color = color;
+  
+  }
+
+  window.location.href = "stage1.html";
+
+}
+
